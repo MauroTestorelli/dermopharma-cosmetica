1f665c511e2582722376541b633d466aee5353f1
MauroTestorelli <maurotestorelli@gmail.com>
2021-06-10 23:48:32 -0300

    Carga con Boots
    Se cargo bootstrap react y se separo navbar como componente

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from "react";
 
+import NavBar from './components/NavBar/NavBar'
+
 class navBar extends React.Component {
   render() {
     return (
-      <React.Fragment>
-      <header> 
-        <img src="images/logo.jpg"/>
-      </header>
-      <nav className="navbar navbar-expand-sm bg-light navbar-light sticky-top">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-responsive" aria-controls="#navbar-responsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbar-responsive">
-        <ul className="nav navbar-nav">
-            <li className="nav-item"><a className="nav-link active" href="index.html">Inicio</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/productos.html">Productos</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/nosotros.html">Nosotros</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/puntos-venta.html">Puntos de Venta</a></li>
-            <li className="nav-item"><a className="nav-link" href="pages/contacto.html">Contacto</a></li> 
-        </ul>
+        <div>
+            <div>
+            </div>
+            <div>
+              <NavBar/>
+            </div>
         </div>
-      </nav>
-      </React.Fragment>
-    ); // href todavia no hecho - Tengo la pagina ya armada, por eso estan los href- Tambien faltaria el diseño del bootstrap, ya que tenia hecho el navbar con boots.
+    ); // href todavia no hecho - Tengo la pagina ya armada, por eso estan los href- No logre colocar el icono del carrito en el Navbar. 
   }
 }
 
